6cde91bd364fc85e9d23b4658e46a7d89a4480bc
jubhavsar <juhi_bhavsar@yahoo.com>
2022-01-31 21:44:57 -0500

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+### Update the first store
+# 1.Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# 2.Load the second store from the database and assign it to @store2.
+# 3.Update the first store (@store1) instance in the database. (Change its name or something.)
+
+#return the store id=1
+@store1 = Store.find_by(id: 1)
+#return the second store
+@store2 = Store.second
+# update first store
+@store1.update(name: "Shershah")
